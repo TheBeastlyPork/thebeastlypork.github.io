cfc4c0ccac1cad2c3b0ff7f808585e5143d36d03
Luke <lukebeaudoin@gmail.com>
2026-06-10 11:46:27 -0400

    First Site Upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 import { defineConfig } from "astro/config";
 
+import remarkMath
+from "remark-math";
+
+import rehypeKatex
+from "rehype-katex";
+
 export default defineConfig({
-  site: "https://thebeastlypork.github.io",
+
+markdown:{
+
+remarkPlugins:[
+remarkMath
+],
+
+rehypePlugins:[
+rehypeKatex
+]
+
+}
+
 });
---
+++ src/content/writing/first-post.md
@@ -0,0 +1,9 @@
+---
+title: First Post
+---
+
+Hello world!
+
+\[
+\dot(x)=x(1-x/N)
+\]
